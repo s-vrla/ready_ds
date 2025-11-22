a12576d508dee1615cbb89f0d9d03c7102d0c5a0
SVrla <sarah.vrla@gmail.com>
2025-11-22 17:36:48 -0500

    uv venv practice

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from ready-ds!")
+import seaborn as sns
+import matplotlib.pyplot as plt
+# Sample data
+data = sns.load_dataset("penguins").dropna()
 
-
-if __name__ == "__main__":
-    main()
+# Plot using seaborn only
+sns.scatterplot(
+data=data, x="flipper_length_mm", y="body_mass_g"
+)
+plt.title("Flipper Length vs Body Mass by Species")
+plt.show()
